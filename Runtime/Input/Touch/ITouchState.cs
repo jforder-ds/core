ca39dfd2329b37ff21ac1bec38a0ec3c71a3b1f5
Forder <45433464+jforder-ds@users.noreply.github.com>
2022-10-20 17:24:05 +0100

    Delete Runtime directory

--- Runtime/Input/Touch/ITouchState.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-
-public interface ITouchState
-{
-    public void Begin(Vector2 input);
-    public void Touch(Vector2 input);
-    public void End(Vector2 input);
-}
--- Runtime/SceneManagement/ScenePicker.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using UnityEngine;
-
-[Serializable]
-public class ScenePicker : MonoBehaviour
-{
-    public string scenePath;
-}
